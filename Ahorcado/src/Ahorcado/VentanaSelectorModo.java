0ee389cbb1d74e52e5055169a663f28c4fe85ae9
Mikel Gonzalez <mikel.g.matos@opendeusto.es>
2023-10-18 16:04:23 +0200

    Estructura de la funcionalidad de los botones

--- Ahorcado/src/Ahorcado/VentanaSelectorModo.java
+++ Ahorcado/src/Ahorcado/VentanaSelectorModo.java
@@ -2,6 +2,9 @@ package Ahorcado;
 
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,6 +17,44 @@ public class VentanaSelectorModo extends JFrame{
 	JButton modoClasico = new JButton("Modo Clasico");
 	
 	public VentanaSelectorModo(){	
+		//ACCIONES DE CADA BOTON (PARA ABIR EL RESTO DE VENATANAS)
+		modoContrarreloj.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		modoMuerteSubita.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		modo1vs1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		modoClasico.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		//DISEÑO DE LA VENTAN
 		JPanel panel= new JPanel(); 
 		panel.setLayout(new GridLayout(4,1));
 		
