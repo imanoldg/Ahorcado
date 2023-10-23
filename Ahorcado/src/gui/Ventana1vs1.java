88f34f47e587abde7f50ebee0768965c26c833b1
Mikel Gonzalez <mikel.g.matos@opendeusto.es>
2023-10-23 12:30:09 +0200

    base del diseño de la ventana 1vs1

--- Ahorcado/src/gui/Ventana1vs1.java
+++ Ahorcado/src/gui/Ventana1vs1.java
@@ -1,5 +1,22 @@
 package gui;
 
-public class Ventana1vs1 {
+import java.awt.GridLayout;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Ventana1vs1 extends JFrame{
+	public Ventana1vs1() {
+		JPanel panel= new JPanel(); 
+		
+		
+		this.add(panel);
+		this.setTitle("Selector de Modo de Juego");
+		this.setSize(800, 600);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+	
+	}
 }
