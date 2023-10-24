4c4bdcbabf9e688be0a033e07688e62412813c9e
Mikel Gonzalez <mikel.g.matos@opendeusto.es>
2023-10-24 14:35:48 +0200

    Diseño de la ventana 1vs1 e implementacion de las instrucciones

--- Ahorcado/src/gui/Ventana1vs1.java
+++ Ahorcado/src/gui/Ventana1vs1.java
@@ -2,10 +2,17 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Ventana1vs1 extends JFrame{
 	private JButton botonA = new JButton("A");
@@ -64,91 +71,186 @@ public class Ventana1vs1 extends JFrame{
 	private JButton botonY2 = new JButton("Y");
 	private JButton botonZ2 = new JButton("Z");
 	
-	private JButton botonSalir = new JButton("  Volver al selector de modo  ");
-	private JButton botonPalabraNueva = new JButton("  Palabra Nueva  ");
-	private JButton botonPalabraNueva2 = new JButton("  Palabra Nueva  ");
+	private JButton botonSalirIzquierda = new JButton("  Volver al selector de modo  ");
+	private JButton botonPalabraNuevaIzquierda = new JButton("  Palabra Nueva  ");
+	
+	private JButton botonSalirDerecha = new JButton("  Volver al selector de modo  ");
+	private JButton botonPalabraNuevaDerecha = new JButton("  Palabra Nueva  ");
 	
 	private int contador;
 	
 	public Ventana1vs1() {
 		JPanel panelGeneral = new JPanel();
 		panelGeneral.setLayout(new GridLayout(1,2));
-		JPanel panelIzquierda = new JPanel();
-		JPanel panelAbajo = new JPanel();
-		panelAbajo.setLocation(0,50);
-		JPanel panelDerecha = new JPanel();
 		
-		panelIzquierda.add(botonA);
-		panelIzquierda.add(botonB);
-		panelIzquierda.add(botonC);
-		panelIzquierda.add(botonD);
-		panelIzquierda.add(botonE);
-		panelIzquierda.add(botonF);
-		panelIzquierda.add(botonG);
-		panelIzquierda.add(botonH);
-		panelIzquierda.add(botonI);
-		panelIzquierda.add(botonJ);
-		panelIzquierda.add(botonK);
-		panelIzquierda.add(botonL);
-		panelIzquierda.add(botonM);
-		panelIzquierda.add(botonN);
-		panelIzquierda.add(botonÑ);
-		panelIzquierda.add(botonO);
-		panelIzquierda.add(botonP);
-		panelIzquierda.add(botonQ);
-		panelIzquierda.add(botonR);
-		panelIzquierda.add(botonS);
-		panelIzquierda.add(botonT);
-		panelIzquierda.add(botonU);
-		panelIzquierda.add(botonV);
-		panelIzquierda.add(botonW);
-		panelIzquierda.add(botonX);
-		panelIzquierda.add(botonY);
-		panelIzquierda.add(botonZ);
+		JPanel panelGeneralIzquierda = new JPanel();
+		panelGeneralIzquierda.setLayout(new GridLayout(2,1));
+		JPanel panelArribaIzquierda = new JPanel();
+		panelArribaIzquierda.setLayout(new GridLayout(1,2));
+		JPanel panelIzquierdaIzquierda = new JPanel();
+		panelIzquierdaIzquierda.setLayout(new GridLayout(6,1));
+		JPanel panelDerechaIzquierda = new JPanel();
+		JPanel panelAbecedarioIzquierda = new JPanel();
+		panelAbecedarioIzquierda.setLayout(new GridLayout(3,10));
 		
-		panelAbajo.add(panelIzquierda);
+		JLabel contadorErroresIzquierda = new JLabel();
+		JLabel erroresIzquierda = new JLabel("ERRORES:");
+		JLabel palabraOcultaIzquierda = new JLabel("_ _ _ _ _");
+		JLabel palabraIzquierda = new JLabel("PALABRA OCULTA:");
+		contadorErroresIzquierda.setHorizontalAlignment(SwingConstants.CENTER);
+		erroresIzquierda.setHorizontalAlignment(SwingConstants.CENTER);
+		palabraOcultaIzquierda.setHorizontalAlignment(SwingConstants.CENTER);
+		palabraIzquierda.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		panelDerecha.add(botonA2);
-		panelDerecha.add(botonB2);
-		panelDerecha.add(botonC2);
-		panelDerecha.add(botonD2);
-		panelDerecha.add(botonE2);
-		panelDerecha.add(botonF2);
-		panelDerecha.add(botonG2);
-		panelDerecha.add(botonH2);
-		panelDerecha.add(botonI2);
-		panelDerecha.add(botonJ2);
-		panelDerecha.add(botonK2);
-		panelDerecha.add(botonL2);
-		panelDerecha.add(botonM2);
-		panelDerecha.add(botonN2);
-		panelDerecha.add(botonÑ2);
-		panelDerecha.add(botonO2);
-		panelDerecha.add(botonP2);
-		panelDerecha.add(botonQ2);
-		panelDerecha.add(botonR2);
-		panelDerecha.add(botonS2);
-		panelDerecha.add(botonT2);
-		panelDerecha.add(botonU2);
-		panelDerecha.add(botonV2);
-		panelDerecha.add(botonW2);
-		panelDerecha.add(botonX2);
-		panelDerecha.add(botonY2);
-		panelDerecha.add(botonZ2);
+		JPanel panelGeneralDerecha = new JPanel();
+		panelGeneralDerecha.setLayout(new GridLayout(2,1));
+		JPanel panelArribaDerecha = new JPanel();
+		panelArribaDerecha.setLayout(new GridLayout(1,2));
+		JPanel panelIzquierdaDerecha = new JPanel();
+		panelIzquierdaDerecha.setLayout(new GridLayout(6,1));
+		JPanel panelDerechaDerecha = new JPanel();
+		JPanel panelAbecedarioDerecha = new JPanel();
+		panelAbecedarioDerecha.setLayout(new GridLayout(3,10));
 		
-		panelAbajo.add(panelDerecha);
+		JLabel contadorErroresDerecha = new JLabel();
+		JLabel erroresDerecha = new JLabel("ERRORES:");
+		JLabel palabraOcultaDerecha = new JLabel("_ _ _ _ _");
+		JLabel palabraDerecha = new JLabel("PALABRA OCULTA:");
+		contadorErroresDerecha.setHorizontalAlignment(SwingConstants.CENTER);
+		erroresDerecha.setHorizontalAlignment(SwingConstants.CENTER);
+		palabraOcultaDerecha.setHorizontalAlignment(SwingConstants.CENTER);
+		palabraDerecha.setHorizontalAlignment(SwingConstants.CENTER);
 		
+		//BARRA DE MENU
+		JMenuBar menu = new JMenuBar();
+		JMenu juego = new JMenu("Juego");
+		
+		JMenu opciones = new JMenu("Opciones");
+		
+		JMenuItem cerrarSesion = new JMenuItem("Cerrar Sesion");
+		JMenuItem cambiarFondo = new JMenuItem("Cambiar fondo del Juego");
+		JMenuItem ranking = new JMenu("Ranking");
+		JMenuItem instrucciones = new JMenu("Instrucciones");
+		
+		juego.add(ranking);
+		juego.add(instrucciones);
+		opciones.add(cerrarSesion);
+		opciones.add(cambiarFondo);
+
+				
+		menu.add(juego);
+		menu.add(opciones);
+
+		instrucciones.addMouseListener(new MouseAdapter() {
+						
+			@Override
+			public void mousePressed(MouseEvent e) {
+				new VentanaInstrucciones1vs1();
+				
+			}
+		});
+		
+		
+		panelAbecedarioIzquierda.add(botonA);
+		panelAbecedarioIzquierda.add(botonB);
+		panelAbecedarioIzquierda.add(botonC);
+		panelAbecedarioIzquierda.add(botonD);
+		panelAbecedarioIzquierda.add(botonE);
+		panelAbecedarioIzquierda.add(botonF);
+		panelAbecedarioIzquierda.add(botonG);
+		panelAbecedarioIzquierda.add(botonH);
+		panelAbecedarioIzquierda.add(botonI);
+		panelAbecedarioIzquierda.add(botonJ);
+		panelAbecedarioIzquierda.add(botonK);
+		panelAbecedarioIzquierda.add(botonL);
+		panelAbecedarioIzquierda.add(botonM);
+		panelAbecedarioIzquierda.add(botonN);
+		panelAbecedarioIzquierda.add(botonÑ);
+		panelAbecedarioIzquierda.add(botonO);
+		panelAbecedarioIzquierda.add(botonP);
+		panelAbecedarioIzquierda.add(botonQ);
+		panelAbecedarioIzquierda.add(botonR);
+		panelAbecedarioIzquierda.add(botonS);
+		panelAbecedarioIzquierda.add(botonT);
+		panelAbecedarioIzquierda.add(botonU);
+		panelAbecedarioIzquierda.add(botonV);
+		panelAbecedarioIzquierda.add(botonW);
+		panelAbecedarioIzquierda.add(botonX);
+		panelAbecedarioIzquierda.add(botonY);
+		panelAbecedarioIzquierda.add(botonZ);
+		
+		
+		panelAbecedarioDerecha.add(botonA2);
+		panelAbecedarioDerecha.add(botonB2);
+		panelAbecedarioDerecha.add(botonC2);
+		panelAbecedarioDerecha.add(botonD2);
+		panelAbecedarioDerecha.add(botonE2);
+		panelAbecedarioDerecha.add(botonF2);
+		panelAbecedarioDerecha.add(botonG2);
+		panelAbecedarioDerecha.add(botonH2);
+		panelAbecedarioDerecha.add(botonI2);
+		panelAbecedarioDerecha.add(botonJ2);
+		panelAbecedarioDerecha.add(botonK2);
+		panelAbecedarioDerecha.add(botonL2);
+		panelAbecedarioDerecha.add(botonM2);
+		panelAbecedarioDerecha.add(botonN2);
+		panelAbecedarioDerecha.add(botonÑ2);
+		panelAbecedarioDerecha.add(botonO2);
+		panelAbecedarioDerecha.add(botonP2);
+		panelAbecedarioDerecha.add(botonQ2);
+		panelAbecedarioDerecha.add(botonR2);
+		panelAbecedarioDerecha.add(botonS2);
+		panelAbecedarioDerecha.add(botonT2);
+		panelAbecedarioDerecha.add(botonU2);
+		panelAbecedarioDerecha.add(botonV2);
+		panelAbecedarioDerecha.add(botonW2);
+		panelAbecedarioDerecha.add(botonX2);
+		panelAbecedarioDerecha.add(botonY2);
+		panelAbecedarioDerecha.add(botonZ2);
+		
+		panelIzquierdaIzquierda.add(botonPalabraNuevaIzquierda);
+		panelIzquierdaIzquierda.add(botonSalirIzquierda);
+		panelIzquierdaIzquierda.add(palabraIzquierda);
+		panelIzquierdaIzquierda.add(palabraOcultaIzquierda);
+		panelIzquierdaIzquierda.add(erroresIzquierda);
+		panelIzquierdaIzquierda.add(contadorErroresIzquierda);
+		
+		panelArribaIzquierda.add(panelIzquierdaIzquierda, BorderLayout.WEST);
+		panelArribaIzquierda.add(panelDerechaIzquierda, BorderLayout.EAST);
+		
+		panelGeneralIzquierda.add(panelArribaIzquierda, BorderLayout.NORTH);
+		panelGeneralIzquierda.add(panelAbecedarioIzquierda, BorderLayout.SOUTH);
+		
+		panelIzquierdaDerecha.add(botonPalabraNuevaDerecha);
+		panelIzquierdaDerecha.add(botonSalirDerecha);
+		panelIzquierdaDerecha.add(palabraDerecha);
+		panelIzquierdaDerecha.add(palabraOcultaDerecha);
+		panelIzquierdaDerecha.add(erroresDerecha);
+		panelIzquierdaDerecha.add(contadorErroresDerecha);
+		
+		panelArribaDerecha.add(panelIzquierdaDerecha, BorderLayout.WEST);
+		panelArribaDerecha.add(panelDerechaDerecha, BorderLayout.EAST);
+				
+		
+		panelGeneralDerecha.add(panelArribaDerecha, BorderLayout.NORTH);
+		panelGeneralDerecha.add(panelAbecedarioDerecha, BorderLayout.SOUTH);
+		
+		
+		panelGeneral.add(panelGeneralDerecha, BorderLayout.EAST);
+		panelGeneral.add(panelGeneralIzquierda, BorderLayout.WEST);
+
+		
+		this.add(panelGeneral, BorderLayout.CENTER);
+		this.setJMenuBar(menu);
 		
 		this.setTitle("1 vs 1");
-		this.setSize(1280, 720);
+		this.setSize(2000, 1200);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		this.setResizable(false);
+		this.setResizable(true);
 		this.setLocationRelativeTo(null);
-		this.setLayout(new GridLayout(1, 2));
-		this.add(panelDerecha, BorderLayout.SOUTH);
-		this.add(panelIzquierda, BorderLayout.SOUTH);
-		this.add(panelAbajo);
+		
+		
 	
 	}
 	
---
+++ Ahorcado/src/gui/VentanaInstrucciones1vs1.java
@@ -0,0 +1,26 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class VentanaInstrucciones1vs1 extends JFrame {
+	public VentanaInstrucciones1vs1() {
+		JLabel instrucciones = new JLabel("INSTRUCCIONES MODO 1vs1");
+		
+		JPanel panel= new JPanel(); 
+		
+		panel.add(instrucciones);
+		
+		this.add(panel);
+		this.setTitle("Instrucciones del Modo Contrarreloj");
+		this.setSize(800, 600);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+	}
+	public static void main(String[] args) {
+		new VentanaInstrucciones1vs1();
+	}
+}
