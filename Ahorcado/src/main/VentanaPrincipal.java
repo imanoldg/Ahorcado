dd0dd715e14d4c24095f2b2c443f413b68a28a25
Mikel Gonzalez <mikel.g.matos@opendeusto.es>
2023-10-26 11:15:53 +0200

    Implementacion de la ventana pricipal

--- Ahorcado/src/main/VentanaPrincipal.java
+++ Ahorcado/src/main/VentanaPrincipal.java
@@ -1,7 +1,9 @@
 package main;
 
+import gui.VentanaUsuarios;
+
 public class VentanaPrincipal {
 	public static void main(String[] args) {
-		System.out.println("es una prueba");
+		new VentanaUsuarios();
 	}
 }
