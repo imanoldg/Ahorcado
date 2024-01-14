1bdcd78d03481ed9c6a447f7383b8ec7909fd576
Imanol Dominguez <ima.dominguez@opendeusto.es>
2024-01-14 22:18:07 +0100

    modificada base de datos y clase usuario

--- Ahorcado/src/domain/Usuario.java
+++ Ahorcado/src/domain/Usuario.java
@@ -4,24 +4,21 @@ public class Usuario {
 	public int cod;
 	public String nombre;
 	public String contraseña;
-	public int puntuacion;
-	
+	public int puntuacionClasico;
+	public int puntuacionContrarreloj;
+	public int puntuacionSubita;
+
 	//Constructor con argumentos
-	public Usuario(int cod, String nombre, String contraseña, int puntuacion) {
+	
+	public Usuario(int cod, String nombre, String contraseña, int puntuacionClasico, int puntuacionContrarreloj,
+			int puntuacionSubita) {
 		super();
 		this.cod = cod;
 		this.nombre = nombre;
 		this.contraseña = contraseña;
-		this.puntuacion = puntuacion;
-	}
-	
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+		this.puntuacionClasico = puntuacionClasico;
+		this.puntuacionContrarreloj = puntuacionContrarreloj;
+		this.puntuacionSubita = puntuacionSubita;
 	}
 
 	public int getCod() {
@@ -32,6 +29,14 @@ public class Usuario {
 		this.cod = cod;
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 	public String getContraseña() {
 		return contraseña;
 	}
@@ -40,17 +45,34 @@ public class Usuario {
 		this.contraseña = contraseña;
 	}
 
-	public int getPuntuacion() {
-		return puntuacion;
+	public int getPuntuacionClasico() {
+		return puntuacionClasico;
 	}
 
-	public void setPuntuacion(int puntuacion) {
-		this.puntuacion = puntuacion;
+	public void setPuntuacionClasico(int puntuacionClasico) {
+		this.puntuacionClasico = puntuacionClasico;
+	}
+
+	public int getPuntuacionContrarreloj() {
+		return puntuacionContrarreloj;
+	}
+
+	public void setPuntuacionContrarreloj(int puntuacionContrarreloj) {
+		this.puntuacionContrarreloj = puntuacionContrarreloj;
+	}
+
+	public int getPuntuacionSubita() {
+		return puntuacionSubita;
+	}
+
+	public void setPuntuacionSubita(int puntuacionSubita) {
+		this.puntuacionSubita = puntuacionSubita;
 	}
 
 	@Override
 	public String toString() {
-		return "Usuario: " + nombre  + "; Puntuacion: " + puntuacion;
+		return String.format("Usuario: %s, Puntuacion Modo Clasico: %d ; Puntuacion Modo Contrarreloj: %d ; Puntuacion Modo Muerte Subita: %d", 
+				nombre, puntuacionClasico, puntuacionContrarreloj, puntuacionSubita);
 	}
 	
 	
--- Ahorcado/test/test/TestUsuario.java
+++ Ahorcado/test/test/TestUsuario.java
@@ -2,24 +2,27 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import domain.Usuario;
 
 public class TestUsuario {
 	
+	private Usuario u;
+	
+	@Before
+	public void setUp() {
+		u = new Usuario(1,"Imanol", "test", 20 , 40, 50);
+	}
+	
 	@Test
 	public void testGetNombre() {
-		Usuario u = new Usuario(1,"Imanol", "test", 20 );
-	
 		assertEquals("Imanol", u.getNombre());
-		
 	}
 	
 	@Test
 	public void testSetNombre() {
-		Usuario u = new Usuario(1,"Imanol", "test", 20 );
-		
 		u.setNombre("Carlos");
 		
 		assertEquals("Carlos", u.getNombre());
@@ -27,15 +30,11 @@ public class TestUsuario {
 	
 	@Test
 	public void testGetCod() {
-		Usuario u = new Usuario(1,"Imanol", "test", 20 );
-		
 		assertEquals(1, u.getCod());
 	}
 	
 	@Test
 	public void testSetCod() {
-		Usuario u = new Usuario(1,"Imanol", "test", 20 );
-		
 		u.setCod(2);
 		
 		assertEquals(2, u.getCod());
@@ -43,41 +42,48 @@ public class TestUsuario {
 	
 	@Test
 	public void testGetContraseña() {
-		Usuario u = new Usuario(1,"Imanol", "test", 20 );
-		
 		assertEquals("test", u.getContraseña());
 	}
 	
 	@Test
 	public void testSetContraseña() {
-		Usuario u = new Usuario(1,"Imanol", "test", 20 );
-		
 		u.setContraseña("prueba");
 		
 		assertEquals("prueba", u.getContraseña());
 	}
 	
 	@Test
-	public void testGetPuntuacion() {
-		Usuario u = new Usuario(1,"Imanol", "test", 20);
-	
-		assertEquals(20, u.getPuntuacion());
+	public void testGetPuntuacionClasico() {
+		assertEquals(20, u.getPuntuacionClasico());
 	}
 	
 	@Test
-	public void testSetPuntuacion() {
-		Usuario u = new Usuario(1,"Imanol", "test", 20);
+	public void testSetPuntuacionClasico() {
+		u.setPuntuacionClasico(100);
 		
-		u.setPuntuacion(100);
-		
-		assertEquals(100, u.getPuntuacion());
+		assertEquals(100, u.getPuntuacionClasico());
 	}
 	
 	@Test
-	public void testToString() {
-		Usuario u = new Usuario(1,"Imanol", "test", 20 );
-		
-		assertEquals("Usuario: Imanol; Puntuacion: 20", u.toString());
+	public void testGetPuntuacionContrarreloj() {
+		assertEquals(40, u.getPuntuacionContrarreloj());
 	}
-
+	
+	@Test
+	public void testSetPuntuacionContrarreloj() {
+		u.setPuntuacionContrarreloj(100);
+		
+		assertEquals(100, u.getPuntuacionContrarreloj());
+	}
+	
+	@Test
+	public void testGetPuntuacionSubita() {
+		assertEquals(50, u.getPuntuacionSubita());
+	}
+	
+	@Test
+	public void testSetPuntuacionSubita() {
+		u.setPuntuacionSubita(400);
+		assertEquals(400, u.getPuntuacionSubita());
+	}
 }
