f796f984da585ba3564d59ce495dec92853c00ed
Imanol Dominguez <ima.dominguez@opendeusto.es>
2024-01-15 22:20:46 +0100

    Ranking modo clasico terminado

--- Ahorcado/src/gui/VentanaRankingClasico.java
+++ Ahorcado/src/gui/VentanaRankingClasico.java
@@ -1,31 +1,38 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.util.ArrayList;
+import java.awt.Color;
+import java.awt.Component;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
 
 import DataBase.MetodosBD;
 import domain.Usuario;
+import io.ComparadorRanking;
 
-public class VentanaRankingClasico extends JFrame{
+public class VentanaRankingClasico extends JFrame {
 
 	private static MetodosBD metodos = new MetodosBD();
 	public static List<Usuario> listaUsuarios = metodos.obtenerListaUsuarios();
 
-	
 	public VentanaRankingClasico() {
 		
+		Collections.sort(listaUsuarios, new ComparadorRanking());
+		
 		JTable tabla = new JTable(new TableModel());
-		
+		tabla.setDefaultRenderer(Object.class, new Renderer());
+		tabla.setRowHeight(70);
+
 		JScrollPane scrollpane = new JScrollPane(tabla);
-		
-		
+
 		this.add(scrollpane, BorderLayout.CENTER);
 		this.setTitle("Ranking del Modo Clasico");
 		this.pack();
@@ -34,17 +41,17 @@ public class VentanaRankingClasico extends JFrame{
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 	}
-	
+
 	public static void main(String[] args) {
 		new VentanaRankingClasico();
 		System.out.println(listaUsuarios);
 	}
-	
-	class TableModel extends AbstractTableModel{
 
+	class TableModel extends AbstractTableModel {
 
-		String[] cabecera = {"", "JUGADOR", "PUNTOS"};
 		
+		String[] cabecera = { "", "JUGADOR", "PUNTOS" };
+
 		@Override
 		public int getRowCount() {
 			// TODO Auto-generated method stub
@@ -61,10 +68,14 @@ public class VentanaRankingClasico extends JFrame{
 		public Object getValueAt(int rowIndex, int columnIndex) {
 
 			switch (columnIndex) {
-			case 0 : return null;
-			case 1: return listaUsuarios.get(rowIndex).getNombre();
-			case 2: return listaUsuarios.get(rowIndex).getPuntuacionClasico();
-			default: return null;
+			case 0:
+				return null;
+			case 1:
+				return listaUsuarios.get(rowIndex).getNombre();
+			case 2:
+				return listaUsuarios.get(rowIndex).getPuntuacionClasico();
+			default:
+				return null;
 			}
 		}
 
@@ -73,9 +84,90 @@ public class VentanaRankingClasico extends JFrame{
 			// TODO Auto-generated method stub
 			return false;
 		}
-		
-		
+
 	}
+
+	class Renderer extends JLabel implements TableCellRenderer {
+
+		public Renderer() {
+			setOpaque(true);
+		}
+
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+				int row, int column) {
+
+			setBackground(new Color(245, 245, 220));
+
+			switch (row) {
+			case 0:
+				switch (column) {
+				case 0:
+					setIcon(new ImageIcon("resources/images/Medalla_1.png"));
+					setText("");
+					setHorizontalAlignment(CENTER);
+					break;
+				default:
+					if (column != 0) {
+						setText(value.toString());
+					} else {
+						setText("");
+					}
+
+					setIcon(null);
+				}
+				setBackground(new Color(255, 215, 0));
+				break;
+
+			case 1:
+				switch (column) {
+				case 0:
+					setIcon(new ImageIcon("resources/images/Medalla_2.png"));
+					setText("");
+					setHorizontalAlignment(CENTER);
+					break;
+				default:
+					if (column != 0) {
+						setText(value.toString());
+					} else {
+						setText("");
+					}
+					setIcon(null);
+				}
+
+				setBackground(new Color(192, 192, 192));
+				break;
+
+			case 2:
+				switch (column) {
+				case 0:
+					setIcon(new ImageIcon("resources/images/Medalla_3.png"));
+					setText("");
+					setHorizontalAlignment(CENTER);
+					break;
+				default:
+					if (column != 0) {
+						setText(value.toString());
+					} else {
+						setText("");
+					}
+					setIcon(null);
+				}
+				setBackground(new Color(210, 105, 30));
+				break;
+
+			default:
+				if (column != 0) {
+					setText(value.toString());
+				} else {
+					setText("");
+				}
+			}
+
+			return this;
+		}
+		
+
+	}
+
 }
-
-
---
+++ Ahorcado/src/io/ComparadorRanking.java
@@ -0,0 +1,15 @@
+package io;
+
+import java.util.Comparator;
+
+import domain.Usuario;
+
+public class ComparadorRanking implements Comparator<Usuario>{
+
+	@Override
+	public int compare(Usuario o1, Usuario o2) {
+		// TODO Auto-generated method stub
+		return (o1.getPuntuacionClasico() - o2.getPuntuacionClasico()) * -1;
+	}
+	
+}
